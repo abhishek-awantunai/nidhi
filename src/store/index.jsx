51dc0dc48aeabaf9218690cc58e228a74172e01d
byheart4u <abhishek.kumar2526@gmail.com>
2022-06-23 18:30:37 +0530

    first commit

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "thunk";
 import imageSlice from './reducers/imageSlice';
 
 const store = configureStore({
     reducer: {
         imagePosts: imageSlice
     }
-});
+}, applyMiddleware(thunk));
 
 export default store;
